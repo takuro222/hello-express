f40b1f731a5623d848dea1dc595092f71bb14750
takuro222 <73879991+takuro222@users.noreply.github.com>
2020-11-05 10:23:28 +0900

    Create server.js

--- server.js
+++ server.js
@@ -1,34 +1,13 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const now = new Date(), y = now.getFullYear(), m = ('0' + (now.getMonth() + 1)).slice(-2);
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/:file", (request, response) => {
+  const path = (request.params.file || '280blocker_adblock.txt').replace('.txt', `_${y}${m}.txt`);
+  response.redirect(`https://280blocker.net/files/${path}`);
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
